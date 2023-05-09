39d1e8b2ee03d7ddc188f17cb658974133494825
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-09 22:45:21 +0530

    Extend Colors in the tailwind.config.js

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1565D8",
+        dark: {
+          soft: "#183B56",
+          hard: "#0D2436",
+        },
+      },
+    },
     fontFamily: {
-      'Ubuntu': ["Ubuntu", "sans-serif"],
-      'babylonica': ['Babylonica', 'cursive'],
-      'fira-sans': ['Fira Sans', 'sans-serif'],
-      'heading': ['Play', 'sans-serif']
+      Ubuntu: ["Ubuntu", "sans-serif"],
+      babylonica: ["Babylonica", "cursive"],
+      "fira-sans": ["Fira Sans", "sans-serif"],
+      heading: ["Play", "sans-serif"],
     },
   },
   plugins: [],
-}
-
+};
