e890ceee8de2dbb29c394dbb5067a7c61c19d5f9
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-10 08:40:18 +0530

    FullyRefactored Toggling nav

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
-const NavItemsInfo =[
-  {name:"Home"},
-  {name:"Articles"},
-  {name:"Pages"},
-  {name:"Pricing"},
-  {name:"Faq"},
-]
+const NavItemsInfo = [
+  { name: "Home" },
+  { name: "Articles" },
+  { name: "Pages" },
+  { name: "Pricing" },
+  { name: "Faq" },
+];
 
 interface HeaderProps {
   name: string;
 }
 
-const NavItem = ({name}: HeaderProps) => {
+const NavItem = ({ name }: HeaderProps) => {
   return (
     <li className="relative group">
       <a href="/" className="px-4 py-2">
@@ -26,18 +28,40 @@ const NavItem = ({name}: HeaderProps) => {
 };
 
 const Header = () => {
+  // define an state visible or unvisible
+  const [navIsVisible, setNavIsVisible] = useState(false);
+
+  // define a handler for toggling my nav is visible state so nav
+  const navVisibilityHandler = () => {
+    setNavIsVisible((curState) => {
+      return !curState;
+    });
+  };
+
   return (
     <section>
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
-        <div>{/* <img src='' alt='logo' /> */}</div>
-        <div className="flex gap-x-9 items-center">
-          <ul className="flex gap-x-2 font-semibold font-Ubuntu">
+        <div>{/* <img className="w-16" src="" alt="logo" /> */}</div>
+        <div className="lg:hidden z-50">
+          {navIsVisible ? (
+            <CloseIcon className="w-6 h-6" onClick={navVisibilityHandler} />
+          ) : (
+            <MenuIcon className="w-6 h-6" onClick={navVisibilityHandler} />
+          )}
+        </div>
+        {/* introducing dynamic class suite string */}
+        <div
+          className={`${
+            navIsVisible ? "right-0" : "-right-full "
+          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+        >
+          <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold font-Ubuntu">
             {/* Use map function to render all the nav items */}
-            {NavItemsInfo.map((item) =>   (
-              <NavItem key={item.name} name={item.name}/>
+            {NavItemsInfo.map((item) => (
+              <NavItem key={item.name} name={item.name} />
             ))}
           </ul>
-          <button className="font-Ubuntu border-2 border-purple-600 px-6 py-2 rounded-full text-purple-600 font-semibold hover:bg-purple-600 hover:text-white transition-all duration-300 text-center">
+          <button className="mt-5 lg:mt-0 font-Ubuntu border-2 border-purple-600 px-6 py-2 rounded-full text-purple-600 font-semibold hover:bg-purple-600 hover:text-white transition-all duration-300 text-center">
             Sign in
           </button>
         </div>
