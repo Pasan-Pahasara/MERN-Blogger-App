8969c52148d9ca56721ce2897eccdd11b0f947e6
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-11 10:46:32 +0530

    HeroSection SearchBar Styles

--- front-end/src/pages/Home/container/Hero/Hero.tsx
+++ front-end/src/pages/Home/container/Hero/Hero.tsx
@@ -1,22 +1,30 @@
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
 import { images } from "../../../../constants/Images";
 
 const Hero = () => {
   return (
     <section className="container mx-auto flex flex-col px-5 py-5 lg:flex-row">
       <div className="mt-10 lg:w-1/2">
-        <h1 className="font-ubuntu text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-4xl xl:text-5xl lg:text-left lg:max-w-[540px]">Read the most interesting articles</h1>
+        <h1 className="font-ubuntu text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-4xl xl:text-5xl lg:text-left lg:max-w-[540px]">
+          Read the most interesting articles
+        </h1>
         <p className="text-dark-light mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga mollitia
           dolorem beatae ipsa, laboriosam assumenda perspiciatis aut culpa ex,
           numquam deleniti distinctio! Eos, doloremque. Laboriosam repudiandae
           cumque amet totam minus?
         </p>
-        <div>
-          <div>
-            <input type="text" />
+        <div className="flex flex-col gap-y-2.5 mt-10 lg:mt-6 xl:mt-10 relative">
+          <div className="relative">
+            <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]" />
+            <input
+              className="placeholder:font-bold placeholder:font-ubuntu font-semibold text-dark-soft placeholder:text-[#959EAD] rounded-lg pl-12 pr-3 w-full py-3 focus:outline-none shadow-2xl shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]  md:py-4"
+              type="text"
+              placeholder="Search article"
+            />
           </div>
-          <button>Search</button>
+          <button className="w-full bg-purple-600 text-white font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2">Search</button>
         </div>
         <div>
           <span>Popular Tags</span>
@@ -27,9 +35,9 @@ const Hero = () => {
           </ul>
         </div>
       </div>
-      
+
       <div className="hidden">
-      <img src={images.HeroImage} alt="users are reading articles" />
+        <img src={images.HeroImage} alt="users are reading articles" />
       </div>
     </section>
   );
