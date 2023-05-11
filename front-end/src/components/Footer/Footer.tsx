0fcd2a62ecf03bde068610c6d6dbc34a15502a93
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-11 23:23:40 +0530

    Added Footer Section

--- front-end/src/components/Footer/Footer.tsx
+++ front-end/src/components/Footer/Footer.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section className="bg-dark-hard">
+      <footer className="container mx-auto grid grid-cols-10 px-5 py-10">
+      </footer>
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
