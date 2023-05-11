401e255a2e4e32b2e94cc37bded1d40afd23fc26
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-11 18:20:42 +0530

    Added to CTA(Call-to-action) Section

--- front-end/src/pages/Home/Home.tsx
+++ front-end/src/pages/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import MainLayout from "../../components/MainLayout/MainLayout";
 import Hero from "./container/Hero/Hero";
 import Articles from "../../components/Articles/Articles";
+import CTA from "./container/CTA/CTA";
 
 const Home = () => {
   return (
     <MainLayout>
       <Hero />
       <Articles />
+      <CTA />
     </MainLayout>
   );
 };
