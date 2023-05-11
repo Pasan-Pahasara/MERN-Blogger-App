b929841e05896b93cb79cff52df12323143421f0
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-11 22:44:47 +0530

    Added Image and FullyRefactored CTA Section

--- front-end/src/constants/Images/images.ts
+++ front-end/src/constants/Images/images.ts
@@ -1,11 +1,13 @@
 import HeroImage from "../../assets/HeroImage.jpg";
 import Post1Image from "../../assets/posts/post1.jpg"
 import PostProfileImage from "../../assets/posts/post-profile.svg"
+import CTAImage from "../../assets/CtaImage.jpg"
 
 const images = {
   HeroImage,
   Post1Image,
-  PostProfileImage
+  PostProfileImage,
+  CTAImage
 };
 
 export default images;
