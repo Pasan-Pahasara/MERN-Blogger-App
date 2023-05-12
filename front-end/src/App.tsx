afe1b8b4b767ddc43e776953d86eca55b80733da
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-12 08:49:28 +0530

    Set Routes In the Home Section

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className="App font-Ubuntu">
+      {/* start routes  */}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      {/* end routes  */}
     </div>
   );
 }
