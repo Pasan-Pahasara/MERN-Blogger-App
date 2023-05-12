c85fc12694e79eebbfc5dd83abc8603d6b2ee2c1
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-12 18:42:35 +0530

    Introducing Comments Components and CommentsContainer Components

---
+++ front-end/src/components/Comments/CommentsContainer/CommentsContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface CommentsContainerProps {
+  className?: string;
+}
+
+const CommentsContainer = ({ className }: CommentsContainerProps) => {
+  return <div className={`${className}`}></div>;
+};
+
+export default CommentsContainer;
