8112567c946ae9d7ca6e4afed6e1eac63170fb9f
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-12 19:14:30 +0530

    CommentsForm Component Added the CommentsContainer

--- front-end/src/components/Comments/CommentsContainer/CommentsContainer.tsx
+++ front-end/src/components/Comments/CommentsContainer/CommentsContainer.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import CommentForm from "../CommentForm/CommentForm";
 
 interface CommentsContainerProps {
   className?: string;
 }
 
 const CommentsContainer = ({ className }: CommentsContainerProps) => {
-  return <div className={`${className}`}></div>;
+  return <div className={`${className}`}>
+    <CommentForm />
+  </div>;
 };
 
 export default CommentsContainer;
+             
