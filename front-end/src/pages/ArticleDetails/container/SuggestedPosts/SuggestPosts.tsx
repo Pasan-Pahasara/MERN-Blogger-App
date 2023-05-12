0898202fcfad68dd41ba5de446250c13963d95f2
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-12 18:28:47 +0530

    Fixing Bugs in the SuggestPosts Section

--- front-end/src/pages/ArticleDetails/container/SuggestedPosts/SuggestPosts.tsx
+++ front-end/src/pages/ArticleDetails/container/SuggestedPosts/SuggestPosts.tsx
@@ -18,7 +18,9 @@ interface CardProps {
 
 const SuggestPosts = ({ className, header, posts = [], tags }: CardProps) => {
   return (
-    <div className={`w-full shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] rounded-lg p-4 ${className}`}>
+    <div
+      className={`w-full shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] rounded-lg p-4 ${className}`}
+    >
       <h2 className="font-Ubuntu font-medium text-dark-hard md:text-xl">
         {header}
       </h2>
@@ -60,9 +62,10 @@ const SuggestPosts = ({ className, header, posts = [], tags }: CardProps) => {
         Tags
       </h2>
       <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
-        {tags.map((item) => {
+        {tags.map((item, index) => {
           return (
             <Link
+              key={index}
               to="/"
               className="inline-block rounded-md px-3 py-1.5 bg-purple-600 font-Ubuntu text-xs text-white md:text-sm"
             >
