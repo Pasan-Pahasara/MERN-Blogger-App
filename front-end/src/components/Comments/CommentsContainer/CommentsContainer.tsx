74f49b94b29924821d3bd2cef8ce0508cf70f82f
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-12 23:21:57 +0530

    Added New Comment data

--- front-end/src/components/Comments/CommentsContainer/CommentsContainer.tsx
+++ front-end/src/components/Comments/CommentsContainer/CommentsContainer.tsx
@@ -3,15 +3,43 @@ import CommentForm from "../CommentForm/CommentForm";
 
 interface CommentsContainerProps {
   className?: string;
+  value: string;
 }
 
 const CommentsContainer = ({ className }: CommentsContainerProps) => {
-  return <div className={`${className}`}>
-    <CommentForm btnLabel="Send" formSubmitHandler={function (comment: string): void {
-      throw new Error("Function not implemented.");
-    } } />
-  </div>;
+  // start add comment
+  const addCommentHandler = (
+    { value }: CommentsContainerProps,
+    parent = null,
+    replyOnUser = null
+  ) => {
+    const newComment = {
+      _id: "10",
+      user: {
+        _id: "a",
+        name: "Oliver Benjamin",
+      },
+      desc: "it was a nice post, Thank you!",
+      post: "1",
+      parent: null,
+      replyOnUser: null,
+      createdAt: "2022-12-31T17:22:05.092+0000",
+    };
+  };
+  // end add comment
+
+  return (
+    <div className={`${className}`}>
+      {/* start added comment form  */}
+      <CommentForm
+        btnLabel="Send"
+        formSubmitHandler={function (comment: string): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+      {/* end added comment form  */}
+    </div>
+  );
 };
 
 export default CommentsContainer;
-             
