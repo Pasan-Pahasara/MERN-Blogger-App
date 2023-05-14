fa62562273390c12e042830483dcfba2dac4692f
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-14 23:57:24 +0530

    Added Reply Button in the comment

--- front-end/src/components/Comments/Comments.tsx
+++ front-end/src/components/Comments/Comments.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import images from "../../constants/Images/images";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 
 interface CommentsProps {
   comment: any;
@@ -7,16 +8,22 @@ interface CommentsProps {
 
 const Comments = ({ comment }: CommentsProps) => {
   return (
+    // start comment wrapper
     <div className="flex flex-nowrap items-start gap-x-3 bg-[#F2F4F5] p-3 rounded-lg">
+      {/* start user profile picture */}
       <img
         src={images.PostProfileImage}
         alt="user profile"
         className="w-9 h-9 object-cover rounded-full"
       />
+      {/* end user profile picture */}
       <div className="flex-1 flex flex-col">
+        {/* start user name */}
         <h5 className="font-bold text-dark-hard text-xs">
           {comment.user.name}
         </h5>
+        {/* end user name */}
+        {/* start commented date */}
         <span className="text-ts text-dark-light">
           {new Date(comment.createdAt).toLocaleDateString("en-US", {
             day: "numeric",
@@ -25,9 +32,23 @@ const Comments = ({ comment }: CommentsProps) => {
             hour: "2-digit",
           })}
         </span>
-        <p>{comment.desc}</p>
+        {/* end commented date */}
+        {/* start user description */}
+        <p className="font-opensans mt-[10px] text-dark-light">
+          {comment.desc}
+        </p>
+        {/* end user description */}
+        <div className="flex items-center gap-x-3 text-dark-light font-Ubuntu text-sm mt-3 mb-3">
+          {/* start reply button  */}
+          <button className="flex items-center space-x-2">
+            <ChatBubbleOutlineIcon className="w-4 h-auto" />
+            <span>Reply</span>
+          </button>
+          {/* end reply button  */}
+        </div>
       </div>
     </div>
+    // end comment wrapper
   );
 };
 
