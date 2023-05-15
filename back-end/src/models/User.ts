69464a286cb641756588d1bd819257bb650f5355
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-05-15 23:21:42 +0530

    Passed the User Properties wrapping a object in the Model package

--- back-end/src/models/User.ts
+++ back-end/src/models/User.ts
@@ -12,4 +12,39 @@ export interface IUser extends Document {
 }
 // end introducing properties
 
+// start passed the properties object 
+const UserSchema = new Schema(
+  {
+    avatar: {
+      type: String,
+      default: "",
+    },
+    name: { 
+        type: String, 
+        required: true, 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+    },
+    password: { 
+        type: String, 
+        required: true, 
+    },
+    verified: { 
+        type: Boolean, 
+        default: false, 
+    },
+    verificationCode: { 
+        type: String, 
+        required: false, 
+    },
+    admin: { 
+        type: Boolean, 
+        default: false, 
+    },
+  },
+  { timestamps: true }
+);
+// end passed the properties object 
 
