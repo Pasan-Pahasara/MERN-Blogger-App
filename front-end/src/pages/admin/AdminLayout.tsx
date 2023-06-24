a32680bda012757dc9c83c53b91a69a0921aeac6
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-24 19:58:44 +0530

    Added AdminLayout page in the front-end

---
+++ front-end/src/pages/admin/AdminLayout.tsx
@@ -0,0 +1,15 @@
+import Header from '../../components/Header/Header';
+import { Outlet } from 'react-router-dom';
+
+function AdminLayout() {
+  return (
+    <div className="flex flex-col h-screen lg:flex-row">
+    <Header />
+    <main className="bg-[#F9F9F9] flex-1 p-4 lg:p-6">
+      <Outlet />
+    </main>
+  </div>
+  )
+}
+
+export default AdminLayout;
