589adcf2b8dc1a27d41d14aecbbfb8bd104d244d
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-24 20:01:03 +0530

    AdminLayout page set route in the front-end

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -7,6 +7,7 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Contact from "./pages/Contact/Contact";
 import About from "./pages/About/About";
+import AdminLayout from "./pages/admin/AdminLayout";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/register" element={<Register />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/admin" element={<AdminLayout />}></Route>
       </Routes>
       {/* end routes  */}
     </div>
