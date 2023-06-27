3fefb6d72da51dc597673d3949ee19bdcde59151
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-27 13:22:18 +0530

    Refactored commentController in the back-end

--- back-end/src/controllers/CommentController.ts
+++ back-end/src/controllers/CommentController.ts
@@ -1,29 +1,36 @@
 import { Request, RequestHandler, Response } from "express";
 import { Comment } from "../models/Comment";
 
-
-export default class CommentController { // CommentController is the controller of the comment
-  createComment: RequestHandler = async ( // createComment is the function to create a comment
+export default class CommentController {
+  // CommentController is the controller of the comment
+  createComment: RequestHandler = async (
+    // createComment is the function to create a comment
     req: Request,
     res: Response
-  ): Promise<Response> => { // Promise<Response> is the return type of the function
+  ): Promise<Response> => {
+    // Promise<Response> is the return type of the function
     try {
       // destructuring assignment
       const { comment } = req.body;
 
       // check whether the relevant comment already exists or not
       let createComment = await Comment.findOne({ comment: comment }); // findOne() is used to find the first document that matches the query
-      if (!createComment) { // save comment only the comment  name is not existing
+      if (!createComment) {
+        // save comment only the comment  name is not existing
         createComment = new Comment({ comment: comment }); // create a new comment
         createComment = await createComment.save(); // save() is used to save the document
 
         return res // return the response
           .status(200)
-          .json({ message: "New comment added.!", responseData: createComment }); 
+          .json({
+            message: "New comment added.!",
+            responseData: createComment,
+          });
       } else {
         return res.status(200).json({ message: "Already exists." });
       }
-    } catch (error: unknown) { // catch block is used to handle the errors
+    } catch (error: unknown) {
+      // catch block is used to handle the errors
       if (error instanceof Error) {
         return res.status(500).json({ message: error.message });
       } else {
@@ -32,15 +39,18 @@ export default class CommentController { // CommentController is the controller 
     }
   };
 
-  retrieveAllComments: RequestHandler = async ( // retrieveAllComments is the function to retrieve all the comments
+  retrieveAllComments: RequestHandler = async (
+    // retrieveAllComments is the function to retrieve all the comments
     req: Request,
     res: Response
-  ): Promise<Response> => { // Promise<Response> is the return type of the function
+  ): Promise<Response> => {
+    // Promise<Response> is the return type of the function
     try {
       let getComments = await Comment.find(); // find() is used to find the documents
       return res.status(200).json({ responseData: getComments }); // return the response
-    } catch (error: unknown) { // catch block is used to handle the errors
-      if (error instanceof Error) { 
+    } catch (error: unknown) {
+      // catch block is used to handle the errors
+      if (error instanceof Error) {
         return res.status(500).json({ message: error.message });
       } else {
         return res.status(500).json({ message: "Unknown error occured." });
@@ -48,22 +58,26 @@ export default class CommentController { // CommentController is the controller 
     }
   };
 
-  updateComment: RequestHandler = async ( // updateComment is the function to update a comment
+  updateComment: RequestHandler = async (
+    // updateComment is the function to update a comment
     req: Request,
     res: Response
-  ): Promise<Response> => { // Promise<Response> is the return type of the function
+  ): Promise<Response> => {
+    // Promise<Response> is the return type of the function
     try {
       // destructuring assignment
       const { id } = req.params; // id is the id of the comment
 
-      let updatedComment = await Comment.findByIdAndUpdate(id, req.body, { // findByIdAndUpdate() is used to find the document by id and update
+      let updatedComment = await Comment.findByIdAndUpdate(id, req.body, {
+        // findByIdAndUpdate() is used to find the document by id and update
         new: true,
       });
       return res // return the response
         .status(200)
-        .json({ message: "Comment updated.", responseData: updatedComment }); 
+        .json({ message: "Comment updated.", responseData: updatedComment });
     } catch (error: unknown) {
-      if (error instanceof Error) { // catch block is used to handle the errors
+      if (error instanceof Error) {
+        // catch block is used to handle the errors
         return res.status(500).json({ message: error.message });
       } else {
         return res.status(500).json({ message: "Unknown error occured." });
@@ -71,22 +85,25 @@ export default class CommentController { // CommentController is the controller 
     }
   };
 
-  deleteComment = async (req: Request, res: Response): Promise<Response> => { // deleteComment is the function to delete a comment
+  deleteComment = async (req: Request, res: Response): Promise<Response> => {
+    // deleteComment is the function to delete a comment
     try {
       // destructuring assignment
       const { id } = req.params; // id is the id of the comment
 
       let deletedComment = await Comment.findByIdAndDelete(id); // findByIdAndDelete() is used to find the document by id and delete
 
-      if (!deletedComment) { // check whether the comment is deleted or not
+      if (!deletedComment) {
+        // check whether the comment is deleted or not
         throw new Error("Failed to delete comment.");
       }
 
       return res // return the response
         .status(200)
         .json({ message: "Comment deleted.", responseData: deletedComment });
-    } catch (error: unknown) { // catch block is used to handle the errors
-      if (error instanceof Error) { 
+    } catch (error: unknown) {
+      // catch block is used to handle the errors
+      if (error instanceof Error) {
         return res.status(500).json({ message: error.message });
       } else {
         return res.status(500).json({ message: "Unknown error occured." });
