81f5607491901de923ba892417462137f3a1f0b4
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-27 12:17:38 +0530

    Refactored Retrieve All Users in the UserController in the back-end

--- back-end/src/controllers/UserController.ts
+++ back-end/src/controllers/UserController.ts
@@ -26,4 +26,22 @@ export default class UserController { // UserController is the controller of the
       return res.status(500).json({ message: "Internal server error" });
     }
   };
+
+  retrieveAllUsers: RequestHandler = async ( // retrieveAllUsers is the function to retrieve all the users
+    req: Request,
+    res: Response
+  ): Promise<Response> => { // Promise<Response> is the return type of the function 
+    //read operation
+    try {
+      let users = await User.find(); // find() is used to find the documents
+
+      return res.status(200).json({ responseData: users });
+    } catch (error: unknown) { // catch block is used to handle the errors
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
 }
