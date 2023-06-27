968fa8cf565d1fb2e19590f3f298e571495d876e
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-27 13:31:51 +0530

    Refactor Routes in the CommentRoutes in the back-end

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import UserRoutes from "./UserRoutes";
 import PostRoutes from "./PostRoutes";
 import CategoryRoutes from "./CategoryRoutes";
+import CommentRoutes from "./CommentRoutes";
 
 const router: Router = Router(); // Router is a class in express
 const url_prefix = "/api/v1"; // url_prefix is the prefix of the url
@@ -9,5 +10,6 @@ const url_prefix = "/api/v1"; // url_prefix is the prefix of the url
 router.use(`${url_prefix}/user`, new UserRoutes().getRouter());
 router.use(`${url_prefix}/post`, new PostRoutes().getRouter());
 router.use(`${url_prefix}/category`, new CategoryRoutes().getRouter());
+router.use(`${url_prefix}/comment`, new CommentRoutes().getRouter());
 
 export default router;
