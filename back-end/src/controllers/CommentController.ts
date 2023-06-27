50863d4c29eba4df1e269c6b3220c25de22e6316
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-27 13:20:55 +0530

    Refactored Retrieve All Comments in the commentController in the back-end

--- back-end/src/controllers/CommentController.ts
+++ back-end/src/controllers/CommentController.ts
@@ -31,4 +31,20 @@ export default class CommentController { // CommentController is the controller 
       }
     }
   };
+
+  retrieveAllComments: RequestHandler = async ( // retrieveAllComments is the function to retrieve all the comments
+    req: Request,
+    res: Response
+  ): Promise<Response> => { // Promise<Response> is the return type of the function
+    try {
+      let getComments = await Comment.find(); // find() is used to find the documents
+      return res.status(200).json({ responseData: getComments }); // return the response
+    } catch (error: unknown) { // catch block is used to handle the errors
+      if (error instanceof Error) { 
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
 }
