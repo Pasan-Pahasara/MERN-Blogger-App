e73960b84d6eb19930acd7253b884628ea603ce1
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-27 12:36:38 +0530

    Added post method in register user in the UserController in the back-end

--- back-end/src/routes/UserRoutes.ts
+++ back-end/src/routes/UserRoutes.ts
@@ -1,17 +1,17 @@
 import express, { Router } from "express";
 import UserController from "../controllers/UserController";
 
-export default class UserRoutes {
-  private router: Router = express.Router();
-  private userController: UserController = new UserController();
+export default class UserRoutes { 
+  private router: Router = express.Router(); // Router is a class in express
+  private userController: UserController = new UserController(); // userController is the instance of the UserController class
 
-  constructor() {
+  constructor() { // constructor() is used to initialize the class
     this.configRoutes();
   }
 
   private configRoutes = (): void => {
-    //POST /api/v1/user
-    this.router.post("./register", this.userController.registerUser);
+    // POST /api/v1/user
+    this.router.post("./", this.userController.registerUser); // registerUser is the function to register a user
   };
 
   public getRouter = (): Router => {
