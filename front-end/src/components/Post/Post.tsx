f462eb6dbc665626b117bc8bddb6143dbb945d43
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-28 14:40:21 +0530

    FullyRefactored Post UI in the front-end

--- front-end/src/components/Post/Post.tsx
+++ front-end/src/components/Post/Post.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from '@mui/icons-material/Edit';
+import EditIcon from "@mui/icons-material/Edit";
 import api from "../../axios";
 
 type PostProps = {
@@ -32,33 +32,86 @@ const deletePost = (postId: String, props: PostProps) => {
 
 const Post: FC<PostProps> = (props) => {
   return (
-    <div className="p-6 border border-slate-400 w-full rounded mt-2">
-      <h3 className="text-center">{props.title}</h3>
-      {props.description}
-      <span className="flex space-x-5">
-        {props.date ? (
-          <p>
-            <strong>Date</strong> : {props.date}
-          </p>
-        ) : null}
-        {props.userName ? (
-          <p>
-            <strong>User Name</strong> : {props.userName}
-          </p>
-        ) : null}
-      </span>
-
-      <div className="w-full flex justify-between items-center">
+    <div className="cursor-pointer p-6 bg-white text-slate-600 space-x-3 rounded-2xl shadow-2xl mt-1">
+      <h3 className="text-center font-bold font-Ubuntu">{props.title}</h3>
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="flex flex-col gap-2 mt-2">
+          {props.date && (
+            <p>
+              <strong>Date</strong> : {props.date}
+            </p>
+          )}
+          {props.userName && (
+            <p>
+              <strong>User Name</strong> : {props.userName}
+            </p>
+          )}
+          {props.categoryId && (
+            <p>
+              <strong>Category</strong> : {props.categoryId}
+            </p>
+          )}
+          {props.tags && (
+            <p>
+              <strong>Tags</strong> : {props.tags.join(", ")}
+            </p>
+          )}
+        </div>
+        <img className="h-10 mt-2" alt="Image" />
+        <div className="w-full grid grid-cols-1 sm:grid-row-2 gap-2">
         <button onClick={() => deletePost(props._id, props)}>
-          <DeleteIcon />
+          <DeleteIcon style={{ color: "#e74c3c" }} />
         </button>
         <button onClick={() => deletePost(props._id, props)}>
-          <EditIcon />
+          <EditIcon style={{ color: "#2ecc71" }} />
         </button>
       </div>
+      </div> */}
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="flex flex-col gap-2 mt-2">
+          {props.date && (
+            <p>
+              <strong>Date</strong> : {props.date}
+            </p>
+          )}
+          {props.userName && (
+            <p>
+              <strong>User Name</strong> : {props.userName}
+            </p>
+          )}
+          {props.categoryId && (
+            <p>
+              <strong>Category</strong> : {props.categoryId}
+            </p>
+          )}
+          {props.tags && (
+            <p>
+              <strong>Tags</strong> : {props.tags.join(", ")}
+            </p>
+          )}
+        </div>
+        <img className="h-10 mt-2" alt="Image" />
+
+        <div className="grid grid-rows-1 sm:grid-rows-2 md:grid-rows-6 justify-end">
+          <button onClick={() => deletePost(props._id, props)}>
+            <DeleteIcon style={{ color: "#e74c3c" }} />
+          </button>
+          <button onClick={() => deletePost(props._id, props)}>
+            <EditIcon style={{ color: "#2ecc71" }} />
+          </button>
+        </div>
+      </div>
+
+      <h2 className="text-center font-semibold font-babylonica">
+        {props.caption}
+      </h2>
+      <p className="flex justify-center items-center font-opensans">
+        {" "}
+        {props.description}
+      </p>
     </div>
   );
 };
 
 export default Post;
-
